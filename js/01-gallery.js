c5f3fa9d3bf5378737bcc7f18eddc13f4a061670
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-10-11 23:28:32 +0300

    Создает и рендерит разметку по массиву galleryItems

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-
 const galleryOfImages = document.querySelector('.gallery');
-console.log(galleryOfImages);
-galleryOfImages.style.width = '100px';
-galleryOfImages.style.height = '80px';
-galleryOfImages.style.backgroundColor = 'black'
+const cardOfImage = onCreateGalleryItem(galleryItems);
 
+galleryOfImages.insertAdjacentHTML('beforeend', cardOfImage);
+
+function onCreateGalleryItem(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+             <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div> 
+        `
+    }).join('')
+}
 galleryOfImages.addEventListener('click', e => {})
