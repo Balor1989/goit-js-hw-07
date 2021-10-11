f6589c0fd80862fb22b095bcf8f5994799c36250
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-10-11 15:29:32 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryOfImages = document.querySelector('.gallery');
+console.log(galleryOfImages);
+galleryOfImages.style.width = '100px';
+galleryOfImages.style.height = '80px';
+galleryOfImages.style.backgroundColor = 'black'
+
+galleryOfImages.addEventListener('click', e => {})
